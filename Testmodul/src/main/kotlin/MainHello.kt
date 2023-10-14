7f0d444a6304ae35630ae740ac335f363e46e7a0
Andreas Voigt <andreas.voigt@innoq.com>
2023-10-14 13:35:46 +0200

    Initial

--- Testmodul/src/main/kotlin/MainHello.kt
+++
@@ -1,9 +0,0 @@
-import org.apache.commons.lang3.StringUtils
-
-object MainHello {
-    @JvmStatic
-    fun main(args: Array<String>) {
-
-        println(StringUtils.upperCase("Kotlin hello"))
-    }
-}
